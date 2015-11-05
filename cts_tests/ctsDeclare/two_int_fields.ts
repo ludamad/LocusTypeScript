935cf555015e10abe73ff9ca4b031a1825a007e2
adomurad <adomurad@localhost.localdomain>
2015-11-04 22:52:44 -0500

    Add reproducer for setter caching problem
    Former-commit-id: e18e7498cc79046a06f06f2e99c2b93cc4c8a3fe

---
+++ cts_tests/ctsDeclare/two_int_fields.ts
@@ -0,0 +1,13 @@
+// @RuntimeTest 
+import {assert, assertNotBranded, assertBranded, assertType, assertFails} from "../cts_asserts";
+
+var test : declare Test = {};
+
+test.x = 0;
+(<any>test).x = 1;
+test.y = 0;
+(<any>test).y = 2;
+
+assert(test.x === 1);
+assert(test.y === 2);
+
