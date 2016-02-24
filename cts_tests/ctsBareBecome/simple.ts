4d02c8cc623e528440f7e92f893c915cbc3227b3
adomurad <adomurad@localhost.localdomain>
2016-02-23 19:26:54 -0500

    More assertions in test

--- cts_tests/ctsBareBecome/simple.ts
+++ cts_tests/ctsBareBecome/simple.ts
@@ -1,5 +1,9 @@
 function addSquareValue(notFooYet: {x: number, y: number} declare HasSquareValue) {
+    /* @assertType("number") */ (notFooYet.x);
+    /* @assertType("number") */ (notFooYet.y);
     notFooYet.square = notFooYet.x*notFooYet.x + notFooYet.y*notFooYet.y;
+    /* @assertType("number") */ (notFooYet.x);
+    /* @assertType("number") */ (notFooYet.y);
 }
 
 let notFooYet = {x: 1, y: 1};
